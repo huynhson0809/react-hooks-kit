b798ccd95ce579a2a2b760085d642d5e69b7cd06
huynhson0809 <huynhson0809@users.noreply.github.com>
2026-07-23 09:15:00 +0700

    feat: add useMediaQuery hook

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { useToggle } from "./useToggle";
 export { useLocalStorage } from "./useLocalStorage";
 export { useDebounce } from "./useDebounce";
 export { useFetch } from "./useFetch";
+export { useMediaQuery } from "./useMediaQuery";
