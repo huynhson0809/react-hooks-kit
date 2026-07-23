059876a3c0fa457ce9020329fba9546b81910004
huynhson0809 <huynhson0809@users.noreply.github.com>
2026-07-23 15:40:00 +0700

    feat: add useClickOutside hook

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { useLocalStorage } from "./useLocalStorage";
 export { useDebounce } from "./useDebounce";
 export { useFetch } from "./useFetch";
 export { useMediaQuery } from "./useMediaQuery";
+export { useClickOutside } from "./useClickOutside";
